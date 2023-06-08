c155e251919c1f93accfec8945df52e4ff37277d
SujayAmberkar <sujayamberkar@gmail.com>
2023-06-08 17:31:37 +0530

    ui fixed

--- Assets/Scripts/SongDataManager/SongUIManager.cs
+++ Assets/Scripts/SongDataManager/SongUIManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SongUIManager : MonoBehaviour
 {
-    public TextMeshProUGUI songNameText;
-    public TextMeshProUGUI artistNameText;
+    public Text songNameText;
+    public Text artistNameText;
     public SongManager songManager;
     public AudioManager audioManager;
+    Song currentSong;
 
     private void Start()
     {
         // Access the song data and update the UI
-        Song currentSong = songManager.songs[audioManager.GetCurrentSong()]; // Assuming you want to display the first song in the list
+        currentSong = songManager.songs[audioManager.GetCurrentSong()]; // Assuming you want to display the first song in the list
+        songNameText.text = currentSong.name;
+        artistNameText.text = currentSong.artist;
+    }
+    
+    private void Update(){
+        currentSong = songManager.songs[audioManager.GetCurrentSong()];
         songNameText.text = currentSong.name;
         artistNameText.text = currentSong.artist;
     }
